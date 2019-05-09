beda4fc2f022d19965ea17d292f92a8a29535e04
Yash <majinsparks@gmail.com>
2019-05-09 21:16:11 +0530

    lkj

--- pi_stream.py
+++ pi_stream.py
@@ -4,20 +4,18 @@ import struct
 from PIL import Image
 import cv2
 import numpy as np
-from visual_odometry import PinholeCamera, VisualOdometry
 from ast import literal_eval
 import urllib.request
 import sys
 import time
 
 orb = cv2.ORB_create(25)
-cam = PinholeCamera(640.0, 480.0, 360.0000, 718.8560, 607.1928, 185.2157)
-vo = VisualOdometry(cam)
-traj = np.zeros((600,600,3), dtype=np.float32)
+
 stream = cv2.VideoCapture(0)
 img_id = 1
 
-#open("log.txt", "w").close()
+# Create some random colors
+color = np.random.randint(0,255,(100,3))
 
 buffer = None
 # Start a socket listening for connections on 0.0.0.0:8000 (0.0.0.0 means
@@ -28,9 +26,13 @@ server_socket.listen(0)
 curx, cury, curz =  0, 0, 0
 prevx, prevy, prevz = 0, 0, 0
 prevImage = None
-OBS_LIMIT = 1
+observations = {}
+# ref_image = cv2.imread("test.jpg")
+# ref_image = cv2.cvtColor(ref_image, cv2.COLOR_BGR2GRAY)
+accept_rate = 0 
 # Accept a single connection and make a file-like object out of it
 connection = server_socket.accept()[0].makefile('rb')
+
 try:
     while True:
         # Read the length of the image as a 32-bit unsigned int. If the
@@ -38,78 +40,68 @@ try:
         image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
         if not image_len:
             break
-        
         try:
             contents = urllib.request.urlopen("http://" + sys.argv[1]).read().decode()
         except:
             contents = None
         if contents == None:
             pass
-        elif buffer != contents:
+        if buffer == contents:
+            accept_rate += 1
+            if accept_rate == 13:
+                print("Current frame for pose generated.")
+                accept_rate = 0
+        if buffer != contents:
             query = literal_eval(contents)
-            # curx = query["pose"][0]
-            # cury = query["pose"][1]
-            # curz = 0
-            # print(x_pos, y_pos, z_pos)
-            # if curx is None and cury is None and curz is None:
-            #     curx = query["pose"][0]
-            #     cury = query["pose"][1]
-            #     curz = 0
-            # else:
+            print(query)
             prevx = curx
             prevy = cury   
             prevz = curz
             curx = query["pose"][0]
             cury = query["pose"][1]
             theta = query["theta"]
+            mleft = query["m_left"]
+            mright = query["m_right"]
+            TS = query["TS"]
+            dtheta = query["dtheta"]
             curz = 0
-            current_data = str(curx) + " " + str(cury) + " " + str(theta) + " "
-            with open("log.txt", "a") as file:
-                file.write(current_data)
-            
-                
-
-                
+            # time.sleep(1)
+            # current_data = str(TS) + " " + str(curx) + " " + str(cury) + " " + str(theta) + " " + str(dtheta) + " " + str(mleft) + " " + str(mright) + " "
+            # with open("log.txt", "a") as file:
+            #     file.write(current_data)
             # print("Previous:", prevx, prevy, prevz)
             # print((query["m_left"] * 100 + query["m_right"] * 100) / 2, query["dtheta"])
         
-
-        # Construct a stream to hold the image data and read the image
-        # data from the connection
+       
+        
         image_stream = io.BytesIO()
         image_stream.write(connection.read(image_len))
-        # Rewind the stream, open it as an image with PIL and do some
-        # processing on it
         image_stream.seek(0)
         image = Image.open(image_stream)
+        
         cv_image = np.flip(np.flip(np.array(image), 0), 1)
-        # cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+        cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+        kp, des = orb.detectAndCompute(cv_image, None)
+        # if (curx, cury, theta) not in observations:
+        #     observations[(curx, cury, theta)] = (kp, des)
+        # else:
+            # create BFMatcher object
+            # bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
+            # # Match descriptors.
+            # matches = bf.match(des, observations[(curx, cury, theta)][1])
 
-        # corners = cv2.goodFeaturesToTrack(cv_image,25,0.01,10)
-        # corners = np.int0(corners)
-
-        
-        # for i in corners:
-        #     x,y = i.ravel()
-        #     current_data += str(x) + " " + str(y) + " "
-        #     cv2.circle(cv_image, (x,y),3,255,-1)
-        
-        cv2.imshow('Stream', cv_image)
-
-        # if buffer != contents:
-        #     with open("log.txt", "a") as file:
-        #         for i in corners:
-        #             x,y = i.ravel()
-        #             file.write(str(x) + " " + str(y) + " ")
-        #     with open("log.txt", "a") as file:
-        #             file.write("-1 -1\n")
-
-    
-
+            # # Sort them in the order of their distance.
+            # matches = sorted(matches, key = lambda x:x.distance)
+            # cv_image = cv2.drawKeypoints(cv_image, observations[(curx, cury, theta)][0], None, color=(0, 255, 0), flags=0)
+            # for match in matches:
+            #     print(match.distance)
+        cv_image = cv2.drawKeypoints(cv_image, kp, cv_image, (0, 255, 0), flags=2)
+        cv2.imshow("Stream", cv_image)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
         buffer = contents
+        prevImage = cv_image
 
 finally:
     connection.close()
