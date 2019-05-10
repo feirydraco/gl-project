d138f26c9920c3170ca5cb41cbe282342258f267
Yash <majinsparks@gmail.com>
2019-05-10 18:45:03 +0530

    asd

--- ekf.py
+++ ekf.py
@@ -5,67 +5,51 @@ import re
 import sympy
 import math
 
-# log = open("log.txt", "r")
+# np.array([
+#             [self.s_t * math.cos(self.s_t + self.theta_t / 2)],
+#             [self.s_t * math.sin(self.s_t + self.theta_t / 2)],
+#             [self.theta_t],
+  
+
+#     def initvalues(self):
+#         with open(self.logfile) as log:
+#             for action in log:
+#                 self.TS = action.split(" ")[0]
+#                 self.obs_x = action.split(" ")[1]
+#                 self.obx_y = action.split(" ")[2]
+#                 self.obs_theta = action.split(" ")[3]
+#                 self.dtheta = action.split(" ")[4]
+#                 self.mleft = action.split(" ")[5]
+#                 self.mright = action.split(" ")[6]
+#                 self.image = action.split(" ")[7]
+#                 break
+
+#     def returnTS(self, TS):
+#         with open(self.logfile) as log:
+#             for actions in log:
+#                 print(actions)
 
 
-class RobotEKF(EKF):
-    def __init__(self, filename):
-        EKF.__init__(self, dim_x = 3, dim_z = 2, dim_u = 2)
-        self.TS = None
-        self.obs_x = None
-        self.obx_y = None
-        self.obs_theta = None
-        self.dtheta = None
-        self.mleft = None
-        self.mright = None
-        self.distance_computed = None
-        self.theta_computed = None
-        self.s_t = None
-        self.theta_t = None
-        self.logfile = filename
-        self.initvalues()
-        B = 5
-        self.setFxuParams()
+# if __name__ == "__main__":
+#     R = RobotEKF("log.txt")
+#     print(R.TS, R.obs_x, R.obx_y)
+#get observation id, frame and values
+def getObs(idx):
+    curr = 0
+    with open("log.txt") as log:
+        for action in log:
+            if curr == idx:
+                return [action.split(" ")[i] for i in range(8)]
+            else:
+                curr += 1
 
-        EKF.F = np.array([
-            [self.s_t * math.cos(self.s_t + self.theta_t / 2)],
-            [self.s_t * math.sin(self.s_t + self.theta_t / 2)],
-            [self.theta_t],
-        ])
-        print(EKF.F)
-        # self.F_j = self.fxu.jacobian(Matrix([x, y, theta]))
-        # self.V_j = self.fxu.jacobian(Matrix([v, a]))
-
-        # # save dictionary and it's variables for later use
-        # self.subs = {x: 0, y: 0, v:0, a:0, 
-        #              time:dt, w:wheelbase, theta:0}
-        # self.x_x, self.x_y, = x, y 
-        # self.v, self.a, self.theta = v, a, theta
-
-    def setFxuParams(self):
-        self.s_t = (float(self.mright) + float(self.mleft)) / 2
-        self.theta_t = (float(self.mright) - float(self.mleft)) / 5
-
-    def initvalues(self):
-        with open(self.logfile) as log:
-            for action in log:
-                self.TS = action.split(" ")[0]
-                self.obs_x = action.split(" ")[1]
-                self.obx_y = action.split(" ")[2]
-                self.obs_theta = action.split(" ")[3]
-                self.dtheta = action.split(" ")[4]
-                self.mleft = action.split(" ")[5]
-                self.mright = action.split(" ")[6]
-                self.image = action.split(" ")[7]
-                break
-
-    def returnTS(self, TS):
-        with open(self.logfile) as log:
-            for actions in log:
-                print(actions)
+R = EKF(3, 2, 2)
+# R.x = [0 0 0]
+R.x = np.array([initvalues()[1], initvalues()[2], initvalues()[3]])
+print(getObs(0))
 
 
-if __name__ == "__main__":
-    R = RobotEKF("log.txt")
-    print(R.TS, R.obs_x, R.obx_y)
+
+
+
     
