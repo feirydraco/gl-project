6136ab3b82946b8f5db2480fd95c675d27cbb74d
Yash <majinsparks@gmail.com>
2019-05-10 18:08:03 +0530

    asd

--- ekf.py
+++ ekf.py
@@ -1,29 +1,71 @@
 from filterpy.kalman import ExtendedKalmanFilter as EKF
-from numpy import dot, array, sqrt
+import numpy as np
+import time
+import re
+import sympy
+import math
+
+# log = open("log.txt", "r")
+
 
 class RobotEKF(EKF):
-    def __init__(self, ts, wheelbase, std_vel, std_steer): #ts: timestamp
-        EKF.__init__(self, 3, 6, 2)
-        self.dt = ts
-        self.wheelbase = wheelbase
-        self.std_vel = std_vel
-        self.std_steer = std_steer
+    def __init__(self, filename):
+        EKF.__init__(self, dim_x = 3, dim_z = 2, dim_u = 2)
+        self.TS = None
+        self.obs_x = None
+        self.obx_y = None
+        self.obs_theta = None
+        self.dtheta = None
+        self.mleft = None
+        self.mright = None
+        self.distance_computed = None
+        self.theta_computed = None
+        self.s_t = None
+        self.theta_t = None
+        self.logfile = filename
+        self.initvalues()
+        B = 5
+        self.setFxuParams()
 
-        a, x, y , v, w, theta, time = symbols('a, x, y, v, w, theta, t')
-        d = v*time
-        beta = (d/w)*sympy.tan(a)
-        r = w/sympy.tan(a)
+        EKF.F = np.array([
+            [self.s_t * math.cos(self.s_t + self.theta_t / 2)],
+            [self.s_t * math.sin(self.s_t + self.theta_t / 2)],
+            [self.theta_t],
+        ])
+        print(EKF.F)
+        # self.F_j = self.fxu.jacobian(Matrix([x, y, theta]))
+        # self.V_j = self.fxu.jacobian(Matrix([v, a]))
+
+        # # save dictionary and it's variables for later use
+        # self.subs = {x: 0, y: 0, v:0, a:0, 
+        #              time:dt, w:wheelbase, theta:0}
+        # self.x_x, self.x_y, = x, y 
+        # self.v, self.a, self.theta = v, a, theta
+
+    def setFxuParams(self):
+        self.s_t = (float(self.mright) + float(self.mleft)) / 2
+        self.theta_t = (float(self.mright) - float(self.mleft)) / 5
+
+    def initvalues(self):
+        with open(self.logfile) as log:
+            for action in log:
+                self.TS = action.split(" ")[0]
+                self.obs_x = action.split(" ")[1]
+                self.obx_y = action.split(" ")[2]
+                self.obs_theta = action.split(" ")[3]
+                self.dtheta = action.split(" ")[4]
+                self.mleft = action.split(" ")[5]
+                self.mright = action.split(" ")[6]
+                self.image = action.split(" ")[7]
+                break
+
+    def returnTS(self, TS):
+        with open(self.logfile) as log:
+            for actions in log:
+                print(actions)
+
+
+if __name__ == "__main__":
+    R = RobotEKF("log.txt")
+    print(R.TS, R.obs_x, R.obx_y)
     
-        self.fxu = Matrix(
-            [[x-r*sympy.sin(theta)+r*sympy.sin(theta+beta)],
-             [y+r*sympy.cos(theta)-r*sympy.cos(theta+beta)],
-             [theta+beta]])
-
-        self.F_j = self.fxu.jacobian(Matrix([x, y, theta]))
-        self.V_j = self.fxu.jacobian(Matrix([v, a]))
-
-        # save dictionary and it's variables for later use
-        self.subs = {x: 0, y: 0, v:0, a:0, 
-                     time:ts, w:wheelbase, theta:0}
-        self.x_x, self.x_y, = x, y 
-        self.v, self.a, self.theta = v, a, theta
--- pi_stream.py
+++ pi_stream.py
@@ -9,11 +9,11 @@ import urllib.request
 import sys
 import time
 
-orb = cv2.ORB_create(25)
+orb = cv2.ORB_create(100)
 
 stream = cv2.VideoCapture(0)
 img_id = 1
-
+current_id = 0
 # Create some random colors
 color = np.random.randint(0,255,(100,3))
 
@@ -29,10 +29,8 @@ prevImage = None
 observations = {}
 # ref_image = cv2.imread("test.jpg")
 # ref_image = cv2.cvtColor(ref_image, cv2.COLOR_BGR2GRAY)
-accept_rate = 0 
 # Accept a single connection and make a file-like object out of it
 connection = server_socket.accept()[0].makefile('rb')
-
 try:
     while True:
         # Read the length of the image as a 32-bit unsigned int. If the
@@ -40,19 +38,19 @@ try:
         image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
         if not image_len:
             break
+        image_stream = io.BytesIO()
+        image_stream.write(connection.read(image_len))
+        image_stream.seek(0)
         try:
             contents = urllib.request.urlopen("http://" + sys.argv[1]).read().decode()
+            query = literal_eval(contents)
         except:
             contents = None
         if contents == None:
             pass
         if buffer == contents:
-            accept_rate += 1
-            if accept_rate == 13:
-                print("Current frame for pose generated.")
-                accept_rate = 0
+            pass
         if buffer != contents:
-            query = literal_eval(contents)
             print(query)
             prevx = curx
             prevy = cury   
@@ -70,33 +68,32 @@ try:
             # with open("log.txt", "a") as file:
             #     file.write(current_data)
             # print("Previous:", prevx, prevy, prevz)
-            # print((query["m_left"] * 100 + query["m_right"] * 100) / 2, query["dtheta"])
-        
-       
-        
-        image_stream = io.BytesIO()
-        image_stream.write(connection.read(image_len))
-        image_stream.seek(0)
-        image = Image.open(image_stream)
-        
-        cv_image = np.flip(np.flip(np.array(image), 0), 1)
-        cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
-        kp, des = orb.detectAndCompute(cv_image, None)
-        # if (curx, cury, theta) not in observations:
-        #     observations[(curx, cury, theta)] = (kp, des)
-        # else:
-            # create BFMatcher object
-            # bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+            # print((query["m_left"] * 100 + query["m_right"] * 100) / 2, query["dtheta"])        
+            image = Image.open(image_stream)
+            cv_image = np.flip(np.flip(np.array(image), 0), 1)
+            cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+            cv2.imwrite("./images/{}.jpg".format(current_id), cv_image)
+            current_id += 1
+            current_data = "msg " + str(TS) + " " + str(curx) + " " + str(cury) + " " + str(theta) + " " + str(dtheta) + " " + str(mleft) + " " + str(mright) + " " + str("./images/{}.jpg".format(current_id)) + "\n"
+            with open("log.txt", "a") as file:
+                 file.write(current_data)
 
-            # # Match descriptors.
-            # matches = bf.match(des, observations[(curx, cury, theta)][1])
+            kp, des = orb.detectAndCompute(cv_image, None)
+            # if (curx, cury, theta) not in observations:
+            #     observations[(curx, cury, theta)] = (kp, des)
+            # else:
+                # create BFMatcher object
+                # bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
-            # # Sort them in the order of their distance.
-            # matches = sorted(matches, key = lambda x:x.distance)
-            # cv_image = cv2.drawKeypoints(cv_image, observations[(curx, cury, theta)][0], None, color=(0, 255, 0), flags=0)
-            # for match in matches:
-            #     print(match.distance)
-        cv_image = cv2.drawKeypoints(cv_image, kp, cv_image, (0, 255, 0), flags=2)
+                # # Match descriptors.
+                # matches = bf.match(des, observations[(curx, cury, theta)][1])
+
+                # # Sort them in the order of their distance.
+                # matches = sorted(matches, key = lambda x:x.distance)
+                # cv_image = cv2.drawKeypoints(cv_image, observations[(curx, cury, theta)][0], None, color=(0, 255, 0), flags=0)
+                # for match in matches:
+                #     print(match.distance)
+            cv_image = cv2.drawKeypoints(cv_image, kp, cv_image, (0, 255, 0), flags=2)
         cv2.imshow("Stream", cv_image)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
